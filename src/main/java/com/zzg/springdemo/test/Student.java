438daa1c55663d0e5b4a4f929ec4bee25c85a5b0
张中贵 <89518224+zhang-zhonggui@users.noreply.github.com>
2021-12-20 17:41:38 +0800

    123

---
+++ src/main/java/com/zzg/springdemo/test/Student.java
@@ -0,0 +1,12 @@
+package com.zzg.springdemo.test;
+
+/**
+ * @Author: zzg
+ * @Description: TODO
+ * @DateTime: 2021/11/27 9:58
+ */
+public class Student {
+    public void name() {
+        System.out.println("my name is zhangsan");
+    }
+}
